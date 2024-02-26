023b031471ba8ff7873be6f78d51a5898485eac2
mikhail-bn <mihail.news@gmail.com>
2024-02-27 01:11:49 +0200

    27-02-24

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,16 +1,71 @@
-import React from "react"
+import React, { useState } from "react"
+
 
 export default function Counter() {
-    const count = 0 
-    const formatCount = () => {
+    const [counter, setCounter] = useState(0)
+    const [tags, setTags] = useState(["tag1", "tag2", "tag3"])
+    
+    //const imageUrl = "https://picsum.photos/200"
+
+    function getClasses() {
+        let classes = "badge m-2 "
+        classes += counter === 0 ? "bg-warning" : "bg-primary"
+
+        return classes
+    }
+
+    function handleIncrement() {
+        setCounter(counter + 1)        
+        console.log('counter: ', counter);
+    }
+    function handleDecrement() {
+        setCounter(counter - 1)        
+        console.log('counter: ', counter);
+    }
+
+    function formatCount() {
         return (
-            count===0 ? "empty" : count
+            counter===0 ? "empty" : counter
         )
     }
+    function handleTagChange(id) {
+        setTags(prevState => prevState.filter(tag => tag !== id))
+        console.log(id);
+    }
+    function renderTags() {
+        if (tags.length === 0) {
+            return "There are no any tags"
+        }
+        return (
+            tags.map(tag => (
+                <li key={tag} 
+                    className="btn btn-primary btn-sm m-2"
+                    onClick={() => handleTagChange(tag) }
+                    >
+                    {tag} 
+                </li>
+            ))
+        )
+    }
+
      return (
         <>
-            <span>{ formatCount() }</span>
-            <button>+</button>
+            <ul>
+                {renderTags()}
+            </ul> 
+            <span className={getClasses()}>{ formatCount() }</span>
+            <button 
+                className="btn btn-primary btn-sm m-2" 
+                onClick={ handleIncrement }
+                >
+                +
+            </button>
+            <button 
+                className="btn btn-primary btn-sm m-2" 
+                onClick={ handleDecrement }
+                >
+                -
+            </button>
         </>
     )
 }
