17bc711f7e446fd3608cbae08678526e623bc748
mikhail-bn <mihail.news@gmail.com>
2024-03-01 19:00:34 +0200

    01-03-24

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,11 +2,12 @@ import React, { useState } from "react"
 
 
 export default function Counter() {
+    // useState
     const [counter, setCounter] = useState(0)
-    const [tags, setTags] = useState(["tag1", "tag2", "tag3"])
+    const [tags, setTags] = useState(["tag1", "tag2", "tag3"])   
     
-    //const imageUrl = "https://picsum.photos/200"
 
+    // Динамический вывод классов
     function getClasses() {
         let classes = "badge m-2 "
         classes += counter === 0 ? "bg-warning" : "bg-primary"
@@ -14,24 +15,30 @@ export default function Counter() {
         return classes
     }
 
+    // Функция изменения состояния счетчика (добавление)
     function handleIncrement() {
-        setCounter(counter + 1)        
-        console.log('counter: ', counter);
-    }
-    function handleDecrement() {
-        setCounter(counter - 1)        
+        setCounter(prevState => prevState + 1)        
         console.log('counter: ', counter);
     }
 
+    // Функция изменения состояния счетчика (вычитание)
+    function handleDecrement() {
+        setCounter(prevState => prevState - 1)        
+        console.log('counter: ', counter);
+    }
+
+    // Функция вывода либо числа либо строки "empty" 
     function formatCount() {
         return (
             counter===0 ? "empty" : counter
         )
     }
+    // Функция изменения всего массива удаляющая тот элемент на который нажали
     function handleTagChange(id) {
         setTags(prevState => prevState.filter(tag => tag !== id))
         console.log(id);
     }
+    // Функция проверки отсутствия переменных в массиве и рендер массива
     function renderTags() {
         if (tags.length === 0) {
             return "There are no any tags"
@@ -46,13 +53,48 @@ export default function Counter() {
                 </li>
             ))
         )
+
+        // ИЛИ
+
+        // return tags.length !== 0 ? tags.map(tag => (
+        //     <li key={tag} 
+        //         className="btn btn-primary btn-sm m-2"
+        //         onClick={() => handleTagChange(tag) }
+        //         >
+        //         {tag} 
+        //     </li>
+        // )) : "There are no any tags"
+
+        // ИЛИ
+
+        // return (
+        //     tags.length !== 0 &&
+        //     tags.map(tag => (
+        //         <li key={tag} 
+        //             className="btn btn-primary btn-sm m-2"
+        //             onClick={() => handleTagChange(tag) }
+        //             >
+        //             {tag} 
+        //         </li>
+        //     ))
+        // )
+        
+        
     }
 
+
+    // Условный рендеринг
+
+    if (tags.length !== 0) {
+        return (<ul>{renderTags()}</ul>)
+    }
+
+    
      return (
         <>
-            <ul>
+            {/* <ul>
                 {renderTags()}
-            </ul> 
+            </ul>  */}
             <span className={getClasses()}>{ formatCount() }</span>
             <button 
                 className="btn btn-primary btn-sm m-2" 
